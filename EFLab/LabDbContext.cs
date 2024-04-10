1ace45892b188f568ef29b5ce61f4daee0677f77
Tibor Tóth <toth.tibor@autsoft.hu>
2024-04-10 12:45:35 +0200

    context

---
+++ EFLab/LabDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EFLab.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace EFLab;
+
+public class LabDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=NEPTUN;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Order> Orders => Set<Order>();
+}
